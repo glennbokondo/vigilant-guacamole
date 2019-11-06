1a11f4af823574b8f280e383297a2c8cd6ea1ceb
Glenn Bokondo <glennbokondo@gmail.com>
2019-11-06 16:48:18 +0100

    remove auto-generated specs

--- GuacAttack/src/app/app.component.spec.ts
+++ GuacAttack/src/app/app.component.spec.ts
@@ -1,35 +1,44 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { TestBed, async } from "@angular/core/testing";
+import { RouterTestingModule } from "@angular/router/testing";
+import { AppComponent } from "./app.component";
+import { MaterialModule } from "./shared/material.module";
+import { AppModule } from "./app.module";
+import { ToolbarComponent } from "./components/toolbar/toolbar.component";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFireModule } from "@angular/fire";
+import { environment } from "src/environments/environment";
+import { AngularFireDatabaseModule } from "@angular/fire/database";
+import { AngularFireStorageModule } from "@angular/fire/storage";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        MaterialModule,
+        AngularFireModule.initializeApp(
+          environment.firebaseConfig,
+          "guacattack"
+        ),
+        AngularFireAuthModule,
+        AngularFireDatabaseModule,
+        AngularFireStorageModule,
+        AngularFirestoreModule
       ],
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent, ToolbarComponent]
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  it("should create the app", async () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'GuacAttack'`, () => {
+  it(`should have as title 'Spectrum'`, async () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('GuacAttack');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('GuacAttack app is running!');
+    expect(app.title).toEqual("Spectrum");
   });
 });
--- GuacAttack/src/app/components/profile/profile.component.spec.ts
+++ GuacAttack/src/app/components/profile/profile.component.spec.ts
@@ -1,25 +1,58 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { ProfileComponent } from './profile.component';
+import { ProfileComponent } from "./profile.component";
+import { MaterialModule } from "src/app/shared/material.module";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { AppModule } from "src/app/app.module";
+import { DebugElement } from "@angular/core";
+import { SkillSelectComponent } from "../skill-select/skill-select.component";
+import { AngularFireModule } from "@angular/fire";
+import { environment } from "src/environments/environment";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFireDatabaseModule } from "@angular/fire/database";
+import { AngularFireStorageModule } from "@angular/fire/storage";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { RouterTestingModule } from '@angular/router/testing';
 
-describe('ProfileComponent', () => {
+describe("ProfileComponent", () => {
   let component: ProfileComponent;
   let fixture: ComponentFixture<ProfileComponent>;
+  let de: DebugElement;
+  let stub: any;
+
+
+  let spy: jasmine.Spy;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProfileComponent ]
-    })
-    .compileComponents();
+      imports: [
+        RouterTestingModule,
+        MaterialModule,
+        ReactiveFormsModule,
+        FormsModule,
+        AngularFireModule.initializeApp(
+          environment.firebaseConfig,
+          "guacattack"
+        ),
+        AngularFireAuthModule,
+        AngularFireDatabaseModule,
+        AngularFireStorageModule,
+        AngularFirestoreModule
+      ],
+      declarations: [ProfileComponent, SkillSelectComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProfileComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
